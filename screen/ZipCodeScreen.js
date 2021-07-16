300300d6c9ec8968681af45fee53b07d20feec49
6210110291 <6210110291@psu.ac.th>
2021-07-16 20:51:10 +0700

    Part5-2

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { FlatList, View, Text, StyleSheet } from 'react-native';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 
 const availableZipItems = [
     { place: 'Hatyai', code: '90110' },
@@ -9,18 +11,24 @@ const availableZipItems = [
     { place: 'Chonburi', code: '20000' },
 ]
 
-const ZipItem = ({place, code}) => (
-    <View style = {styles.zipItem}>
-        <Text>{place}</Text>
-        <Text>{code}</Text>
-    </View>
+const ZipItem = ({place, code, navigation}) => (
+    <TouchableHighlight onPress = {() => {
+        navigation.navigate('Weather', {zipCode: code})
+    }}>
+
+        <View style = {styles.zipItem}>
+            <Text>{place}</Text>
+            <Text>{code}</Text>
+        </View>
+    </TouchableHighlight>
 )
 export default function ZipCodeScreen() {
+    const navigation = useNavigation();
     return (
         <FlatList
             data = {availableZipItems}
             keyExtractor = {item => item.code}
-            renderItem = {({item}) => <ZipItem {...item}/>}
+        renderItem = {({item}) => <ZipItem {...item} navigation={navigation}/>}
         />
     )
 }
