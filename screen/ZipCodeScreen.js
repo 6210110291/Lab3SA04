cb164f34c0efa0863381d60d19e35b4df75597e8
6210110291 <6210110291@psu.ac.th>
2021-07-17 18:57:12 +0700

    Part6-6

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -7,11 +7,12 @@ const Dev_Height = Dimensions.get("window").height
 const Dev_Width = Dimensions.get("window").width
 
 const availableZipItems = [
+    { place: 'Bangkok', code: '10110' },
+    { place: 'Khon Kaen', code: '40000' },
+    { place: 'Chiang Mai', code: '50000' },
     { place: 'Hatyai', code: '90110' },
     { place: 'Trang', code: '92000' },
-    { place: 'Chiang Mai', code: '50000' },
-    { place: 'Khon Kaen', code: '40000' },
-    { place: 'Bangkok', code: '10110' },
+    { place: 'Yala', code: '95000' },
 ]
 
 const ZipItem = ({place, code, navigation}) => (
@@ -31,7 +32,9 @@ export default function ZipCodeScreen() {
         <SafeAreaView style={styles.container}>
             <StatusBar translucent={true} backgroundColor="#000"/>
                 <ImageBackground source = {require('../bg2.jpg')} style = {styles.backdrop}>
-                <Text style = {styles.text}>What is weather today ?</Text>
+                <Text style = {styles.text1}>What is</Text>
+                <Text style = {styles.text2}>Weather</Text>
+                <Text style = {styles.text3}>Today ?</Text>
                     <FlatList
                         data = {availableZipItems}
                          keyExtractor = {item => item.code}
@@ -49,32 +52,44 @@ const styles = StyleSheet.create ({
     },
     zipItem: {
         flex: 1,
-        height:"80%",
-        width:"90%",
-        backgroundColor: 'rgba(255, 255, 255, 0.4)',
-        borderRadius:20,
+        height:"100%",
+        width:"80%",
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderRadius:10,
         alignItems:"center",
         flexDirection:"row",
-        marginLeft:"5%",
-        marginTop:"5%",
+        marginLeft:"10%",
+        marginTop:"3.5%",
     },
-    text: {
-        fontSize:25,
-        color:"#FFF",
+    text1: {
+        fontSize:30,
+        color:"#fffacd",
         fontWeight:"bold",
-        marginLeft:"7%",
+        marginLeft:"10%",
         marginTop:"5%"
+    },
+    text2: {
+        fontSize:50,
+        color:"#f0f0f0",
+        fontWeight:"bold",
+        marginLeft:"15%",
+    },
+    text3: {
+        fontSize:33,
+        color:"#fffacd",
+        fontWeight:"bold",
+        marginLeft:"10%",
     },
     place_text: {
         flex: 1,
         fontSize:20,
-        color:"#FFF",
+        color:"#464646",
         marginLeft:"5%",
     },
     code_text: {
         flex: 1,
         fontSize:20,
-        color:"#FFF",
+        color:"#464646",
         marginLeft:"5%",
     },
     backdrop: {
